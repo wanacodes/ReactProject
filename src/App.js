31e828a8268f659f1e123fd06a490c8cdb8d4140
Wan Firdaus <wanfirdaus91@gmail.com>
2019-09-25 17:08:14 +0800

    start building portfolio

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React from "react";
 import "./App.css";
-import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
+import { Layout, Header, Navigation, Content } from "react-mdl";
 import Main from "./components/main";
 import { Link } from "react-router-dom";
 
 function App() {
     return (
         <div style={{ height: "300px", position: "relative" }}>
-            <Layout
-                style={{
-                    background:
-                        "url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover"
-                }}
-            >
+            <Layout fixedHeader>
                 <Header
-                    transparent
-                    title="My Portfolio"
-                    style={{ color: "white" }}
+                    title={
+                        <span>
+                            <span style={{ color: "#ddd" }}>Area / </span>
+                            <strong>The Title</strong>
+                        </span>
+                    }
                 >
                     <Navigation>
                         <Link to="/resume">Resume</Link>
@@ -25,14 +23,6 @@ function App() {
                         <Link to="/contact">Contacts</Link>
                     </Navigation>
                 </Header>
-                <Drawer title="My Portfolio">
-                    <Navigation>
-                        <Link to="/resume">Resume</Link>
-                        <Link to="/aboutme">About Me</Link>
-                        <Link to="/projects">Projects</Link>
-                        <Link to="/contact">Contacts</Link>
-                    </Navigation>
-                </Drawer>
                 <Main />
                 <Content />
             </Layout>
